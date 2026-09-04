68051c03ab5773a9442ed9e71f8a3ca4ea5088b1
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-04 00:19:57 -0600

    Complete exercise 10 and 11 - JS foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperatureF) {
+  let temperatureC = (temperatureF - 32) * (5 / 9);
+  return Math.round(temperatureC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperatureC) {
+  let temperatureF = (9 * temperatureC) / 5 + 32;
+  return Math.round(temperatureF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
