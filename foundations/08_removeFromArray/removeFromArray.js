68c564c4c2f7c18ffffd0969166d209edd56ee89
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-03 23:44:59 -0600

    Complete exercises 8 and 9

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  //   args.forEach((item) => {
+  //     while (array.includes(item)) {
+  //       index = array.indexOf(item);
+  //       index === -1 || array.splice(index, 1);
+  //     }
+  //   });
+
+  return array.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  let [start, end] = num1 < num2 ? [num1, num2] : [num2, num1];
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
