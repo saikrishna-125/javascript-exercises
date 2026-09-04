e5e408c8a4320d60b53acf4b90574ad3be6a02a5
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-03 22:55:55 -0600

    Compelete exercises 6 and 7 in JS foundations

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  //   return string.split("").reverse().join("");
+  let reversedString = "";
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string.at(i);
+  }
+
+  return reversedString;
 };
 
 // Do not edit below this line
