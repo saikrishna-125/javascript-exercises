55a3f62a5c2ee34ba3958947447ee0beab71476b
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-06 14:47:37 -0600

    Add solution for foundations 12, 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  return nums.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+  return nums.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  result = 1;
+
+  if (num === 0) {
+    return 1;
+  }
+
+  while (num > 0) {
+    result *= num;
+    num -= 1;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  let filtered = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumeric.includes(char));
+
+  let actual = filtered.join("");
+  let reverse = filtered.reverse().join("");
+  return actual === reverse;
 };
 
 // Do not edit below this line
