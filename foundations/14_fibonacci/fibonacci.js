fff6d1d18977da4d99a682687ffafbbb645d5856
Sai Krishna <sai.k.karnam@gmail.com>
2026-09-06 15:16:25 -0600

    Add answers for foundations 14, 15, 16

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let prev = 1;
+  let current = 1;
+  let temp;
 
+  num = +num;
+
+  if (num < 0) {
+    return "OOPS";
+  } else if (num < 2) {
+    return num;
+  }
+
+  for (let i = 2; i < num; i++) {
+    temp = current;
+    current = prev + current;
+    prev = temp;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce(
+    (oldest, person) => {
+      yearOfDeath = person.yearOfDeath || new Date().getFullYear();
 
+      age = yearOfDeath - person.yearOfBirth;
+
+      person.age = age;
+
+      return age > oldest.age ? person : oldest;
+    },
+    { age: 0 },
+  );
 };
 
 // Do not edit below this line
